c2f669bce8da39b7499917832a3c748bc94d88ce
cakalita <cakalita@users.noreply.github.com>
2020-01-23 10:22:45 -0500

    Update and rename decaf_run.sh to decaf_run_bygene.sh
    out to out_folder, --indiv flag

--- decaf_run_bygene.sh
+++ decaf_run_bygene.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 30                # Number of cores (processes/threads?)
+#SBATCH -n 10                # Number of cores (processes/threads?)
 #SBATCH -N 1                # Ensure that all cores are on one machine; number of nodes
 #SBATCH --mem=20000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o job_readouts/myoutput_%j.out  #"outFile"$1".txt"# File to which STDOUT will be written, %j inserts jobid
@@ -20,7 +20,7 @@ echo started ${CHR}
 #need matrix of variant info, genotypes, and ref/alt counts, all split by space and then I think it splits lines?
 
 mkdir ${DATA}/stratas_results
-mkdir ${DATA}/stratas_results/${OUT}
+mkdir ${DATA}/stratas_results/${OUT_FOLDER}
 
 #NOTE that peak file needs to be preprocessed to have columns: CHR     P0      P1      NAME    CENTER
 
@@ -30,7 +30,7 @@ echo ${DATA}/CNV/${LOCAL}
 echo ${GROUP}/LNCaP/knownGene_withAlias.stratas.chr${CHR}.txt 
 echo ${CELL}
 echo ${DATA}/${EXPRESS}
-echo ${DATA}/stratas_results/${OUT}
+echo ${DATA}/stratas_results/${OUT_FOLDER}
 
 Rscript slurm_src/stratas_DeCAF.R \
 	--input ${DATA}/stratas_input_files/${SAMPLE}.chr${CHR}.OUT.MAT.split \
@@ -42,9 +42,10 @@ Rscript slurm_src/stratas_DeCAF.R \
 	--pheno FALSE \
 	--gene_express ${DATA}/${EXPRESS} \
 	--bbreg TRUE \
-	--data_path ${DATA}/stratas_results/${OUT} \
+	--data_path ${DATA}/stratas_results/${OUT_FOLDER} \
 	--betabinom ${BINOM} \
 	--sample_name ${SAMPLE}.chr${CHR}.${GENE} \
+	--indiv ${INDIV} \
 	--gene_name ${GENE} #for running a single gene
 
 termin=$(date +"%s")
