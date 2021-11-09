237bf613b6c017b6a5f95e0f3ca7a8dea0898dea
cakalita <cakalita@users.noreply.github.com>
2021-11-09 15:46:59 -0500

    Update batch_decaf.sh

--- batch_decaf.sh
+++ batch_decaf.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-#SBATCH -n 10                # Number of cores (processes/threads?)
-#SBATCH -N 1                # Ensure that all cores are on one machine; number of nodes
-#SBATCH --mem=20000           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o job_readouts/myoutput_%j.out  #"outFile"$1".txt"# File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e job_readouts/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
-#SBATCH --mail-type=ALL
+#SBATCH -n 6                # Number of cores (processes/threads?)
+#SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=cakalita@gmail.com
 
 begin=$(date +"%s")
 
+#mem used is less if not running all tests
+#old mem set to 12000
+#old outputting
+# -o job_readouts/myoutput_%j.out  #"outFile"$1".txt"# File to which STDOUT will be written, %j inserts jobid
+# -e job_readouts/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+
 #work from home folder
 #stored big files in group
-GROUP="../../agusevlab"
+GROUP="/agusevlab"
 
 #load modules
-module load R/3.5.2 
+module load R/4.0 
+module load htslib
+
 echo started ${GENE_SET}
 echo ${OUT_FOLDER}
-
-#need matrix of variant info, genotypes, and ref/alt counts, all split by space and then I think it splits lines?
-#this script runs stratas on a subset of data 
 
 mkdir ${DATA}/stratas_results
 mkdir ${DATA}/stratas_results/${OUT_FOLDER}
@@ -32,39 +34,68 @@ CHR=$(basename ${GENE_SET} | rev | cut -d"." -f2 | rev )
 echo $set_num
 echo $CHR
 
-#run this before running script
-#In agusevlab/LNCaP/split_gene
-#for chr in {1..22}; do 
-#to split gene file into chromosomes 
-#	awk -v OFS='\t' '{print > ("../knownGene_withAlias.stratas."$1".txt")}' ../knownGene_withAlias.stratas.txt
-#to split chr gene file into smaller parts
-#	split -d -l 150 ../knownGene_withAlias.stratas.chr${chr}.txt knownGene_withAlias.stratas.chr${chr}.
-#done
-#also run for matrix file
-#awk -v OFS='\t' '{print > ("KIRC.ALL.AS."$1".OUT.MAT.split")}' KIRC.ALL.AS.OUT.MAT.split
+if [ ${SAMPLE} == "BRCA" ]; then
+OUT="${DATA}/stratas_input_files/${SAMPLE}.${set_num}"
+else
+OUT="${DATA}/stratas_input_files/small/${SAMPLE}.${set_num}"
+fi
+echo $OUT 
 
-Rscript slurm_src/stratas_sasha_mc_gene_local.R \
-	--input ${DATA}/stratas_input_files/${SAMPLE}.${CHR}.OUT.MAT.split \
+if [ ! -s "$OUT.mat" ]; then
+	echo $OUT.mat is empty
+	exit
+else
+
+echo input is $OUT.mat
+echo peaks is ${GENE_SET}
+echo data path is ${DATA}/stratas_results/${OUT_FOLDER}
+
+#if [ -z ${RERUN+x} ]; then RERUN="FALSE"; else echo "var is set to '$RERUN'"; fi
+if [ "$RERUN" = TRUE ] && [ "${CELLSPECIFIC}" = TRUE ] && [ ! -s "${DATA}/stratas_results/${OUT_FOLDER}${SAMPLE}.${set_num}.cfQTL.bbreg_stratas_results.txt" ]; then
+RERUN="FALSE"
+fi
+
+
+NUM_CPU_CORES=$(nproc --all)
+memlimit=$(ulimit -m)
+memlimit_adj=$(($memlimit-1000000))
+ulimit -u $(($NUM_CPU_CORES/4 * 3)) #Limit "X" process to 50% CPU usage
+#ulimit -v $memlimit_adj #Limit "X" process to 50% CPU usage
+ulimit -m $memlimit_adj
+ulimit -a
+
+
+Rscript slurm_src/decaf_nocnv.R \
+	--input $OUT.mat \
 	--global_param ${DATA}/stratas_input_files/${GLOBAL} \
 	--local_param ${DATA}/CNV/${LOCAL} \
 	--peaks ${GENE_SET} \
-	--cell_specific TRUE \
+	--cell_specific ${CELLSPECIFIC} \
 	--cell_pop ${CELL} \
 	--pheno FALSE \
 	--gene_express ${DATA}/${EXPRESS} \
-	--bbreg TRUE \
+	--bbreg ${BBREG} \
+	--eqtl_vanilla ${EQTL_VANILLA} \
+	--bbreg_vanilla ${EQTL_VANILLA} \
+	--dopurity ${DOPURITY} \
+	--doprs ${DOPRS} \
+	--prs ${PRS} \
+	--dointeraction ${DOINTERACTION} \
+	--purity ${DATA}/${PURITY} \
 	--data_path ${DATA}/stratas_results/${OUT_FOLDER} \
 	--betabinom ${BINOM} \
 	--indiv ${INDIV} \
+	--rerun ${RERUN} \
+	--ranknorm FALSE \
+	--window ${WINDOW} \
 	--sample_name ${SAMPLE}.${set_num}
-#--gene_name DPF3 #for running a single gene
+#	--gene_name ${GENE} \
+
+#	--ranknorm TRUE #default is set to false 
+
 
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
 echo "$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) seconds elapsed for Script Execution. #Completed: ${set_num}"
 
-#example run:
-#for gene_set in ../../agusevlab/LNCaP/split_gene/*chr3*+([0-9]); do
-#  sbatch --job-name=${gene_set} --export=BINOM="TRUE",INDIV="TRUE",LOCAL="KIRC.ALL.AS.CNVLOCAL",GLOBAL="KIRC.ALL.AS.nona.global.params",SAMPLE="KIRC.ALL.AS.01A",DATA="../../agusevlab/ckal/TCGA_vcf/KIRC",PEAK="LNCaP/split_gene/${gene_set}",GENE_SET="${gene_set}",CELL="../../agusevlab/ckal/AIsim/KIRC_TIMER.txt",OUT_FOLDER="test_batch/",EXPRESS="broad_rnaseq/KIRC.rnaseq.fixedsamples.full.txt" slurm_src/batch_decaf.sh 
-#  sleep 1
-#done
+fi
